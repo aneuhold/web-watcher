baf151d3df2a5a4911cb1249715fd25a7c2baa2c
Anton G Neuhold Jr <agneuhold@gmail.com>
2022-02-26 16:54:03 -0800

    Fix up basic eslint setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import fetch from 'node-fetch';
+
 console.log('This is a test');
+
+async function testFunction() {
+  const googleSite = await fetch('https://www.google.com');
+  console.log(googleSite.body);
+}
+
+testFunction();
